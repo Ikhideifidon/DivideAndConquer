d1ed977e680109aac4a4abb36ceef8859489cdf0
ifidonikhide <ikhideifidon@yahoo.com>
2022-11-29 11:07:20 -0500

    DivideConquer

--- src/main/java/com/Github/IkhideIfidon/ListNode.java
+++ src/main/java/com/Github/IkhideIfidon/ListNode.java
@@ -15,13 +15,29 @@ public class ListNode {
         return newNode;
     }
 
-    // Function for linked list implementation from the given set of keys
+    /**
+     * The addAll method constructs a new linked list from an array of keys
+     * @param keys : An array of keys
+     * @return ListNode
+     */
     public static ListNode addAll(int[] keys) {
         ListNode head = null;
         // start from the end of the array
         for (int i = keys.length - 1; i >= 0; i--)
             head = push(head, keys[i]);
         return head;
+    }
+
+    /**
+     * The append method concatenates to the end of the linked list a linked list from an array of keys.
+     * @param head : The pointer to the head of the original linked list.
+     * @param keys : An array of keys.
+     */
+    public static void append(ListNode head, int... keys) {
+        ListNode current = head;
+        while (current.next != null)
+            current = current.next;
+        current.next = addAll(keys);
     }
 
     public static void printList(ListNode head) {
--- src/main/java/com/Github/IkhideIfidon/Main.java
+++ src/main/java/com/Github/IkhideIfidon/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         int[] nums = {4, 4, 2, 4, 2, 2, 4, 2, 2, 2};
         System.out.println(DivideConquer.majorityElement(nums));
+        ListNode node = ListNode.addAll(nums);
+        ListNode.append(node, 10, 11, 12);
+        ListNode.printList(node);
     }
 }
